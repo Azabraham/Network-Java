fe7a0deccd8b80238360f8e298264d2351261a23
Azabraham <154074694+Azabraham@users.noreply.github.com>
2025-01-18 13:00:30 -0500

    Basic class structure

--- Packet.java
+++ Packet.java
@@ -1,12 +1,15 @@
 package network;
 
 public class Packet {
+	
 	String sourceIP = null;
 	String destinIP = null;
 	Segment payload = null;
+	
 	public Packet(String sIP, String dIP, Segment payload) {
 		this.sourceIP = sIP;
 		this.destinIP = dIP;
 		this.payload = payload;
 	}
+
 }
